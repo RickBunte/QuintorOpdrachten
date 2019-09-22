7ef69d90e79348007520eb7f1bd599d034bcc294
Rick Bunte <rickbunte2000@gmail.com>
2019-09-22 13:12:42 +0200

    Emptied the master branch

--- src/main/java/program/Greeter.java
+++
@@ -1,7 +0,0 @@
-package Program;
-
-public class Greeter{
-    public String sayHello(){
-        return "Hello World!";
-    }
-}
--- src/main/java/program/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package Program;
-
-public class HelloWorld{
-    public static void Main(String[] args){
-        Greeter greeter = new Greeter();
-        System.out.println(greeter.sayHello());
-        }
-}
